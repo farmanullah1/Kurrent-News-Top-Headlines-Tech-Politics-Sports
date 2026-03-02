bfc9220589ee86909e04a146f71a149ed7d81b7e
Farmanullah <farmanullahansari999@gmail.com>
2026-03-02 17:59:41 +0500

    Update

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
     static defaultProps = {
@@ -18,7 +19,6 @@ export default class News extends Component {
         pageSize: PropTypes.number
     }
 
-  // 1. Removed async, it's just a normal function
   capitalizeFirstLetter = (string) => {
       return string.charAt(0).toUpperCase() + string.slice(1);
   } 
@@ -27,17 +27,16 @@ export default class News extends Component {
     super(props)
     this.state = {
       articles: [],
-      loading: false,
+      loading: true, // Start as true for the initial load
       page: 1,
       totalResults: 0
     }
-    // 2. Change the browser tab title dynamically!
     document.title = `${this.capitalizeFirstLetter(this.props.category)} - Kurrent News`;
   }
 
+  // This runs ONLY for the very first page load
   async updateNews() {
     this.props.setProgress(10); 
-    
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`; 
     this.setState({ loading: true });
     
@@ -60,67 +59,62 @@ export default class News extends Component {
     this.updateNews();
   }
 
-  handlePrevClick = async () => {
-    this.setState({ page: this.state.page - 1 }, () => {
-      this.updateNews();
-    });
-  }
-
-  handleNextClick = async () => {
-    if (this.state.page + 1 <= Math.ceil(this.state.totalResults / this.props.pageSize)) {
-      this.setState({ page: this.state.page + 1 }, () => {
-        this.updateNews();
+  // NEW FUNCTION: This fetches data silently in the background when you scroll down
+  fetchMoreData = async () => {
+    // 1. Increment the page number
+    this.setState({ page: this.state.page + 1 }, async () => {
+      // 2. Fetch the new page
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`; 
+      
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      
+      // 3. Concat (attach) the new articles to the existing articles!
+      this.setState({
+        articles: this.state.articles.concat(parsedData.articles || []),
+        totalResults: parsedData.totalResults
       });
-    }
-  }
+    });
+  };
 
   render() {
     return (
-      <div className='container my-3'>
-        {/* 3. Inject the capitalized category directly into the H2 tag! */}
-        <h2 className="text-center" style={{ margin: '35px 0px' }}>
+      // Removed the 'container' class from this outer div to prevent scrollbar issues
+      <div className='my-3'> 
+        <h2 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>
           Kurrent News - Top {this.capitalizeFirstLetter(this.props.category)} Headlines
         </h2>
         
+        {/* Top spinner for the initial load ONLY */}
         {this.state.loading && <Spinner />}
 
-        <div className='row'>
-          {!this.state.loading && this.state.articles.map((element, index) => {
-            return (
-              <div className='col-md-4' key={element.url || index}>
-                <NewsItems
-                  title={element.title ? element.title.slice(0, 45) : ""}
-                  description={element.description ? element.description.slice(0, 88) : ""}
-                  imageUrl={element.urlToImage ? element.urlToImage : "https://images.macrumors.com/t/RQPLZ_3_iMyj3evjsWnMLVwPdyA=/1600x/article-new/2023/11/apple-pay-feature-dynamic-island.jpg"}
-                  newsUrl={element.url} 
-                  author={element.author}
-                  date={element.publishedAt} 
-                  source={element.source ? element.source.name : "Unknown"}
-                />
-              </div>
-            )
-          })}
-        </div>
-
-        <div className="container d-flex justify-content-between my-4">
-          <button 
-            disabled={this.state.page <= 1} 
-            type="button" 
-            className="btn btn-dark" 
-            onClick={this.handlePrevClick}
-          > 
-            &larr; Previous 
-          </button>
-          
-          <button 
-            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} 
-            type="button" 
-            className="btn btn-dark" 
-            onClick={this.handleNextClick}
-          >
-            Next &rarr;
-          </button>
-        </div>
+        {/* 4. Properly wrapped InfiniteScroll around the grid */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className='row'>
+              {this.state.articles.map((element, index) => {
+                return (
+                  <div className='col-md-4' key={element.url || index}>
+                    <NewsItems
+                      title={element.title ? element.title.slice(0, 45) : ""}
+                      description={element.description ? element.description.slice(0, 88) : ""}
+                      imageUrl={element.urlToImage ? element.urlToImage : "https://images.macrumors.com/t/RQPLZ_3_iMyj3evjsWnMLVwPdyA=/1600x/article-new/2023/11/apple-pay-feature-dynamic-island.jpg"}
+                      newsUrl={element.url} 
+                      author={element.author}
+                      date={element.publishedAt} 
+                      source={element.source ? element.source.name : "Unknown"}
+                    />
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
       </div>
     )
   }
